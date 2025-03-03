085101ae3ec3b3556acc087d9e0351e0d35f5b5c
Nagenir <6283767@cegepmontpetit.ca>
2025-03-03 12:00:46 -0500

    DESIGN_Add css style to Index, Details and Materials of Courses

--- E-Study/E-Study/ViewModels/CourseDisplayVM.cs
+++ E-Study/E-Study/ViewModels/CourseDisplayVM.cs
@@ -1,10 +1,12 @@
 ﻿using E_Study.Models;
+using System.ComponentModel;
 
 namespace E_Study.ViewModels
 {
     public class CourseDisplayVM
     {
         public int Id { get; set; }
+        [DisplayName("Course Title")]
         public string Title { get; set; }
         public CourseDisplayVM(Course course)
         {
